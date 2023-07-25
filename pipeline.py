b0fcf327fa04457e90a30d41f6428e9620efe840
atharva-kurle <atharva@ubuntuc.myguest.virtualbox.org>
2023-07-25 19:39:54 +0530

    added export csv feature

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('titanic.csv')
 
-survived_df = df.loc[df['Survived']=1]
+survived_df = df.loc[df['Survived']==1]
 
 survived_df.to_csv('survivedPassangers.csv')
